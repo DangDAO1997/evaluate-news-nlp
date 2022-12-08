e3d91e87df900f1620b159e232df2d9afe419e0d
trangnguyen2809 <trangnguyennguyen2809@gmail.com>
2022-12-08 16:47:12 +0700

    first commit

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,25 @@
+import {checkForName} from "./nameChecker"
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
+    checkForName(formText)
+    
+    console.log("::: Form Submitted :::")
+    fetch(`http://localhost:8080/news?name=${formText}`)
+    .then(res => res.json())
+    .then(res => {
+        let data = "";
+        for (const e of res) {
+            data += `<br>${e.title}<br>`;
+        }
+        return data;
+    })
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res;
+    })
+}
+
+export { handleSubmit }
